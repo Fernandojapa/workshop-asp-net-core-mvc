48235e9f60ac81cc3e387ab6434471a1acb6973c
Fernando Fernandes <fernando.fernandesffxt@gmail.com>
2024-11-02 17:17:23 -0300

    Add project files.

---
+++ Fazenda Urbana/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fazenda_Urbana.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
