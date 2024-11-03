1f59260647ad312792e8d7ed6bc52520430ac02e
Fernando Fernandes <fernando.fernandesffxt@gmail.com>
2024-11-03 09:18:46 -0300

    CRUD Scaffolding

---
+++ Fazenda Urbana/Data/FazendaUrbanaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FazendaUrbana.Models
+{
+    public class FazendaUrbanaContext : DbContext
+    {
+        public FazendaUrbanaContext (DbContextOptions<FazendaUrbanaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FazendaUrbana.Models.Department> Department { get; set; }
+    }
+}
