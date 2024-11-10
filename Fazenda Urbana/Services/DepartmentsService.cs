d1f139fd1272de1a05f03c0702f51d574090f58c
Fernando Fernandes <fernando.fernandesffxt@gmail.com>
2024-11-10 18:05:04 -0300

    Asynchronous operations using Tasks (async, await)

--- Fazenda Urbana/Services/DepartmentsService.cs
+++ Fazenda Urbana/Services/DepartmentsService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FazendaUrbana.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FazendaUrbana.Services
 {
@@ -15,9 +16,9 @@ namespace FazendaUrbana.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
