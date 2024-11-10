9863fc170877e62f2d77a00644cd43c8709b960d
Fernando Fernandes <fernando.fernandesffxt@gmail.com>
2024-11-10 01:21:55 -0300

    Update seller and custom service exception

--- Fazenda Urbana/Controllers/SellersController.cs
+++ Fazenda Urbana/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using FazendaUrbana.Services;
 using FazendaUrbana.Models;
 using FazendaUrbana.Models.ViewModels;
+using FazendaUrbana.Services.Exceptions;
 
 namespace FazendaUrbana.Controllers
 {
@@ -28,7 +29,7 @@ namespace FazendaUrbana.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var viewModel = new SallerFormViewModel { Departments = departments };
+            var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
 
@@ -77,6 +78,47 @@ namespace FazendaUrbana.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
